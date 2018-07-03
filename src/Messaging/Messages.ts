d255d33a7e6f4daf117db4a14b99133d781ec3a0
Kenneth Lavender <5175120+kennylavender@users.noreply.github.com>
2018-07-02 21:12:42 -0700

    remove unused failure messages

--- src/Messaging/Messages.ts
+++ src/Messaging/Messages.ts
@@ -1,13 +1,10 @@
 export enum Exchange {
   BatchReaderReadNextDirectoryRequest = 'BATCH_READER::READ_NEXT_DIRECTORY_REQUEST',
   BatchReaderReadNextDirectorySuccess = 'BATCH_READER::READ_NEXT_DIRECTORY_SUCCESS',
-  BatchReaderReadNextDirectoryFailure = 'BATCH_READER::READ_NEXT_DIRECTORY_FAILURE',
   BatchWriterCreateNextBatchRequest = 'BATCH_WRITER::CREATE_NEXT_BATCH_REQUEST',
   BatchWriterCreateNextBatchSuccess = 'BATCH_WRITER::CREATE_NEXT_BATCH_SUCCESS',
-  BatchWriterCreateNextBatchFailure = 'BATCH_WRITER::CREATE_NEXT_BATCH_FAILURE',
   BatchWriterCompleteHashesRequest = 'BATCH_WRITER::COMPLETE_HASHES_REQUEST',
   BatchWriterCompleteHashesSuccess = 'BATCH_WRITER::COMPLETE_HASHES_SUCCESS',
-  BatchWriterCompleteHashesFailure = 'BATCH_WRITER::COMPLETE_HASHES_FAILURE',
   BlockchainWriterTimestampRequest = 'BLOCKCHAIN_WRITER::TIMESTAMP_REQUEST',
   BlockchainWriterTimestampSuccess = 'BLOCKCHAIN_WRITER::TIMESTAMP_SUCCESS',
   BlockchainWriterTimestampFailure = 'BLOCKCHAIN_WRITER::TIMESTAMP_FAILURE',
