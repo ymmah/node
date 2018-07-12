da0bb9ca94fbc149ce5fd339ed7f9032ebb5ddce
Kenny Lavender <5175120+kennylavender@users.noreply.github.com>
2018-07-11 21:47:08 -0700

    add try/catch

--- src/BatchWriter/Service.ts
+++ src/BatchWriter/Service.ts
@@ -1,6 +1,8 @@
 import { Interval } from '@po.et/poet-js'
 import { inject, injectable } from 'inversify'
+import * as Pino from 'pino'
 
+import { childWithFileName } from 'Helpers/Logging'
 import { secondsToMiliseconds } from 'Helpers/Time'
 import { Exchange } from 'Messaging/Messages'
 import { Messaging } from 'Messaging/Messaging'
@@ -11,11 +13,14 @@ import { ServiceConfiguration } from './ServiceConfiguration'
 export class Service {
   private readonly interval: Interval
   private readonly messaging: Messaging
+  private readonly logger: Pino.Logger
 
   constructor(
+    @inject('Logger') logger: Pino.Logger,
     @inject('ServiceConfiguration') configuration: ServiceConfiguration,
     @inject('Messaging') messaging: Messaging
   ) {
+    this.logger = childWithFileName(logger, __filename)
     this.messaging = messaging
     this.interval = new Interval(
       this.createNextBatch,
@@ -32,6 +37,11 @@ export class Service {
   }
 
   private createNextBatch = async () => {
-    this.messaging.publish(Exchange.BatchWriterCreateNextBatchRequest, '')
+    const logger = this.logger.child({ method: 'readNextDirectory' })
+    try {
+      await this.messaging.publish(Exchange.BatchWriterCreateNextBatchRequest, '')
+    } catch(error) {
+      logger.error({ error }, 'Uncaught exception in BatchWriter Service')
+    }
   }
 }
