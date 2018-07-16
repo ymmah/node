1b32e9ce3850474f8af6f06b26f3e23f7fcd3027
Jesse Wicks <jessewicks@gmail.com>
2018-07-16 14:56:23 -0500

    lint-fix

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,8 @@ import 'Error'
 import * as Pino from 'pino'
 import 'reflect-metadata'
 
-import 'Extensions/Promise'
 import 'Extensions/Array'
+import 'Extensions/Promise'
 
 import { API } from 'API/API'
 import { BlockchainReader } from 'BlockchainReader/BlockchainReader'
